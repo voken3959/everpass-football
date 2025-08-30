2ed74ba3f3a4e63a9243504acdf24b5361a7938f
voken3959 <jurbergsdeivids@gmail.com>
2025-08-30 18:29:21 +0100

    Update create-checkout.js

--- api/create-checkout.js
+++ api/create-checkout.js
@@ -23,6 +23,7 @@ module.exports = async (req, res) => {
       ],
       success_url: `${req.headers.origin}/success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${req.headers.origin}/cancel`,
+
     });
 
     res.status(200).json({ url: session.url });
